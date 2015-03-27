9888d43d45271f7090ce05ed1ddab7b23930ba57
Noah Crocker <necrocke@umich.edu>
2015-03-27 12:02:05 -0400

    Added makefile tutorial ref and updated genStats to work on OSX

--- genStats.sh
+++ genStats.sh
@@ -3,14 +3,14 @@
 blue=$(tput setaf 4)
 normal=$(tput sgr0)
 
-nrOfTeams=$(find -mindepth 1 -maxdepth 1 -type d | wc -l)
-nrOfTeamsWSubmit=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/home/ncrocker/IA/repos/{} describe 2>&1" | grep -B 1 submission | grep -e "\./.*" | wc -l)
+nrOfTeams=$(gfind -mindepth 1 -maxdepth 1 -type d | wc -l)
+nrOfTeamsWSubmit=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} describe 2>&1" | grep -B 1 submission | grep -e "\./.*" | wc -l)
 #Get number of people who have not committed at all
-nrOfTeamsWNoCommits=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/home/ncrocker/IA/repos/{} rev-parse HEAD 2>&1 | grep fatal | wc -l)
-nrOfSubmissions=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/home/ncrocker/IA/repos/{} tag -l | wc -l)
-submissionAggregates=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/home/ncrocker/IA/repos/{} tag -l | sort | grep -o "2015.[0-9]\+.[0-9]\+" | uniq -c)
-avgLinesInFile=$(find -type f -name pager.c* -exec wc -l {} \; | cut -d ' ' -f1 | awk '{sum+=$1}END{printf "Sum=%d\nCount=%d\nAve=%.2f\n",sum,NR,sum/NR}')
-lastSubmitDates=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/home/ncrocker/IA/repos/{} describe 2>&1" | grep -o "2015.[0-9]\+.[0-9]\+" | sort | uniq -c)
+nrOfTeamsWNoCommits=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} rev-parse HEAD 2>&1 | grep fatal | wc -l)
+nrOfSubmissions=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} tag -l | wc -l)
+submissionAggregates=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} tag -l | sort | grep -o "2015.[0-9]\+.[0-9]\+" | uniq -c)
+avgLinesInFile=$(gfind -type f -name pager.c* -exec wc -l {} \; | cut -d ' ' -f1 | awk '{sum+=$1}END{printf "Sum=%d\nCount=%d\nAve=%.2f\n",sum,NR,sum/NR}')
+lastSubmitDates=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} describe 2>&1" | grep -o "2015.[0-9]\+.[0-9]\+" | sort | uniq -c)
 
 printf "Out of ${blue}%s${normal} teams, ${blue}%s${normal} have made at least one submit\n" "$nrOfTeams" "$nrOfTeamsWSubmit"
 avgSubmissions=$(bc -l <<< "$nrOfSubmissions/$nrOfTeamsWSubmit")

af37ddc91bc876c8fcc3cdaf8130da83d2a5d50a
Noah Crocker <necrocke@umich.edu>
2015-04-23 19:06:24 -0400

    Prepping for move to the EECS482 org

--- README.md
+++ README.md
@@ -4,21 +4,6 @@ A dumping ground for my EECS482 discussion/office hour tips.
 
 You can download non-text files by right clicking "raw" and using the normal save mechanism on your browser (or just clone the repo).
 
-## Scripts ##
-
-Some useful scripts. Add them to your path (on CAEN, you can make a ~/bin folder and add that folder to your path in your ~/.bashrc or ~/.zshrc)  
-
-Use
-
-    export PATH=~/bin:$PATH
-    
-in your RC to add the folder to your path  
-
-I better not see any output of "git-rank-contributers" in the project feedback though...  
-
-git-wtf can be useful for telling what's going on with your remote repo.  
-git-up is a decent replacement for "git pull". It'll pull from remote and then show the log of all the commits you didn't have yet. Might be worth aliasing using your ~/.gitconfig
-
 ## GitCmds.txt ##
 
 I use the commands in this file to generate stats that I hope to mention in discussion section. It might be worth looking at if you want to learn some command-line-foo (or cringe at if you know real command-line-foo. I chain grep like no tomorrow...)
--- genStats.sh
+++ genStats.sh
@@ -3,20 +3,20 @@
 blue=$(tput setaf 4)
 normal=$(tput sgr0)
 
-nrOfTeams=$(gfind -mindepth 1 -maxdepth 1 -type d | wc -l)
-nrOfTeamsWSubmit=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} describe 2>&1" | grep -B 1 submission | grep -e "\./.*" | wc -l)
+nrOfTeams=$(find -mindepth 1 -maxdepth 1 -type d | wc -l)
+nrOfTeamsWSubmit=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} describe 2>&1" | grep -B 1 submission | grep -e "\./.*" | wc -l)
 #Get number of people who have not committed at all
-nrOfTeamsWNoCommits=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} rev-parse HEAD 2>&1 | grep fatal | wc -l)
-nrOfSubmissions=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} tag -l | wc -l)
-submissionAggregates=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} tag -l | sort | grep -o "2015.[0-9]\+.[0-9]\+" | uniq -c)
-avgLinesInFile=$(gfind -type f -name pager.c* -exec wc -l {} \; | cut -d ' ' -f1 | awk '{sum+=$1}END{printf "Sum=%d\nCount=%d\nAve=%.2f\n",sum,NR,sum/NR}')
-lastSubmitDates=$(gfind -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} describe 2>&1" | grep -o "2015.[0-9]\+.[0-9]\+" | sort | uniq -c)
+nrOfTeamsWNoCommits=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} rev-parse HEAD 2>&1 | grep fatal | wc -l)
+nrOfSubmissions=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} tag -l | wc -l)
+submissionAggregates=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} tag -l | sort | grep -o "2015.[0-9]\+.[0-9]\+" | uniq -c)
+avgLinesInFile=$(find -type f -name fs_server.c* -exec wc -l {} \; | cut -d ' ' -f1 | awk '{sum+=$1}END{printf "Sum=%d\nCount=%d\nAve=%.2f\n",sum,NR,sum/NR}')
+lastSubmitDates=$(find -mindepth 1 -maxdepth 1 -type d | xargs -I{} sh -c "echo '{}'; git --git-dir={}/.git --work-tree=/Users/ncrocker/IA/repos/{} describe 2>&1" | grep -o "2015.[0-9]\+.[0-9]\+" | sort | uniq -c)
 
 printf "Out of ${blue}%s${normal} teams, ${blue}%s${normal} have made at least one submit\n" "$nrOfTeams" "$nrOfTeamsWSubmit"
 avgSubmissions=$(bc -l <<< "$nrOfSubmissions/$nrOfTeamsWSubmit")
 printf "There have been ${blue}%s${normal} submissions to date, making the average ${blue}%.4s${normal} submits per team (who have submitted)\n" "$nrOfSubmissions" "$avgSubmissions"
 printf "${blue}%s${normal} have yet to commit anything... Better get started...\n" "$nrOfTeamsWNoCommits"
-printf "The file pager.c* has the following stats: %s\n" $avgLinesInFile
+printf "The file fs_server.c* has the following stats: %s\n" $avgLinesInFile
 printf "The submission histogram is as follows\n"
 echo "$submissionAggregates"
 printf "The last submission dates for all teams is as follows\n"
